c7134e64775a55e52c533284c70c2c2dd93d8e10
Braian Mellor <braianj@gmail.com>
2026-01-06 09:17:39 -0300

    refactor: extract RichText renderers for better maintainability

--- src/components/Blog/RichText/RichText.tsx
+++ src/components/Blog/RichText/RichText.tsx
@@ -1,24 +1,19 @@
 import * as React from 'react'
-import { TwitterTweetEmbed } from 'react-twitter-embed'
 import { type Options, documentToReactComponents } from '@contentful/rich-text-react-renderer'
-import { BLOCKS, type Document, INLINES, type Text } from '@contentful/rich-text-types'
+import { BLOCKS, type Document, INLINES } from '@contentful/rich-text-types'
+import { renderEmbeddedAsset, renderHyperlink } from './RichText.renderers'
 import {
   Blockquote,
-  EmbeddedImage,
   Heading1,
   Heading2,
   Heading3,
   Heading4,
   Heading5,
   Heading6,
-  Hyperlink,
-  LinkedInEmbed,
   ListItem,
   OrderedList,
   Paragraph,
-  TwitterContainer,
-  UnorderedList,
-  YouTubeEmbed
+  UnorderedList
 } from './RichText.styled'
 import type { ContentfulAsset } from '../../../shared/types/blog.domain'
 
@@ -40,61 +35,8 @@ const createRichTextOptions = (assets: Record<string, ContentfulAsset>): Options
     [BLOCKS.OL_LIST]: (_node, children) => <OrderedList>{children}</OrderedList>,
     [BLOCKS.LIST_ITEM]: (_node, children) => <ListItem>{children}</ListItem>,
     [BLOCKS.QUOTE]: (_node, children) => <Blockquote>{children}</Blockquote>,
-    [BLOCKS.EMBEDDED_ASSET]: (node) => {
-      const assetId = node.data?.target?.sys?.id as string | undefined
-      if (assetId && assets[assetId]) {
-        return <EmbeddedImage src={assets[assetId].url} alt="" />
-      }
-      // Fallback: try to get URL directly from node data (for resolved assets)
-      const url = node.data?.target?.fields?.file?.url as string | undefined
-      if (url) {
-        return <EmbeddedImage src={url.startsWith('//') ? `https:${url}` : url} alt="" />
-      }
-      return null
-    },
-    [INLINES.HYPERLINK]: (node) => {
-      const uri = node.data.uri as string
-      const content = node.content as Text[]
-      const contentValue = content[0]?.value
-
-      if ((uri.includes('youtube.com') || uri.includes('youtu.be')) && contentValue === uri) {
-        const url = new URL(uri)
-        const videoCode = url.searchParams.has('v') ? url.searchParams.get('v') : url.pathname.split('/').pop()
-
-        return (
-          <YouTubeEmbed
-            src={`https://www.youtube.com/embed/${videoCode}`}
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          />
-        )
-      }
-
-      if ((uri.includes('twitter.com') || uri.includes('x.com')) && contentValue === uri) {
-        const twitterSplit = uri.split('/')
-        const tweetId = twitterSplit[twitterSplit.length - 1].split('?')[0]
-
-        return (
-          <TwitterContainer>
-            <TwitterTweetEmbed tweetId={tweetId} options={{ theme: 'dark' }} />
-          </TwitterContainer>
-        )
-      }
-
-      if (uri.includes('linkedin.com') && contentValue === uri) {
-        return <LinkedInEmbed src={uri} title="Embedded Linkedin Post" />
-      }
-
-      const isInternalAnchor = uri.startsWith('#') || (uri.startsWith('https://decentraland.org/blog') && uri.includes('#'))
-      const target = isInternalAnchor ? '_self' : '_blank'
-
-      return (
-        <Hyperlink href={uri} target={target} rel="noopener noreferrer">
-          {contentValue}
-        </Hyperlink>
-      )
-    }
+    [BLOCKS.EMBEDDED_ASSET]: (node) => renderEmbeddedAsset(node, assets),
+    [INLINES.HYPERLINK]: (node) => renderHyperlink(node)
   }
 })
 
