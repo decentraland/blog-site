12653dab48691c3bd7812e813867fb3ea56d03b7
Braian Mellor <braianj@gmail.com>
2026-02-17 16:07:35 -0300

    refactor: rename fullWidthContent const by banner (#78)

--- src/components/PageLayout/PageLayout.types.ts
+++ src/components/PageLayout/PageLayout.types.ts
@@ -3,6 +3,6 @@ import type { ReactNode } from 'react'
 export interface PageLayoutProps {
   children: ReactNode
   activeCategory?: string
-  fullWidthContent?: ReactNode
+  banner?: ReactNode
   showBlogNavigation?: boolean
 }
